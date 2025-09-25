a3b0bafa10b43ba341825607f0f02d57015d875e
hchanona <hchanonatv@gmail.com>
2025-09-25 15:09:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ Cada página tiene su propia barra lateral con parámetros.
 
 # --- Definir páginas del multipágina con títulos personalizados ---
 home       = st.Page(instrucciones, title="Instrucciones:")
-cournot    = st.Page("pages/1_Duopolio_de_Cournot.py",          title="Duopolio de Cournot")
-oligo_asim = st.Page("pages/5_Oligopolio_Cournot_Asimetrico.py", title="Oligopolio de Cournot")
-stack      = st.Page("pages/2_Stackelberg_Duopolio.py",         title="Duopolio de Stackelberg")
-hotelling  = st.Page("pages/4_Hotelling_Lineal.py",             title="Hotelling lineal")
-colusion   = st.Page("pages/3_Colusion.py",                     title="Colusión Cournot")
+cournot    = st.Page("pages/1_Duopolio_de_Cournot.py",          title="1. Duopolio de Cournot")
+oligo_asim = st.Page("pages/5_Oligopolio_Cournot_Asimetrico.py", title="2. Oligopolio de Cournot")
+stack      = st.Page("pages/2_Stackelberg_Duopolio.py",         title="3. Duopolio de Stackelberg")
+hotelling  = st.Page("pages/4_Hotelling_Lineal.py",             title="4. Hotelling lineal")
+colusion   = st.Page("pages/3_Colusion.py",                     title="5. Colusión Cournot")
 
 
 # --- Router ---
